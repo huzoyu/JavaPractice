ff8f772d2189fcbd8443b55a9e1f2c1359d58e1a
huzoyu <noahahahah@foxmail.com>
2018-09-27 19:33:51 +0800

    Update Export2Sheet.java

--- src/com/zhongyu/utils/Export2Sheet.java
+++ src/com/zhongyu/utils/Export2Sheet.java
@@ -12,33 +12,76 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
 public class Export2Sheet {
-	private static String iUri = "D:\\T_JKPF_API_DisplayICON_Normal_01.java";
+	private static String projectUri = "D:\\svn\\JDTest\\JD_Z32_Test\\JDPF_Q4B3C_Test\\JDPF_Q4B3C_ABC_TestSuites";
 	private static String oUri = "D:\\sheet.txt";
-
+  
 	public static void main(String[] args){
-		File iFile = new File(iUri);
+		File iFile = new File(projectUri);
 		File oFile = new File(oUri );
 		try {
-			new Export2Sheet().export(iFile, oFile);
+			new Export2Sheet().readFile(iFile, oFile);
 		} catch (Exception e) {
 			// TODO: handle exception
+		}
+	}
+
+	private String category;
+	
+	private void readFile(File iFile, File oFile) throws IOException{
+		if (iFile.isDirectory()) {
+			File[] fileList = iFile.listFiles();
+			for (File file : fileList) {
+				new Export2Sheet().readFile(file, oFile);
+			}
+		}else if (iFile.isFile() && iFile.getName().endsWith(".java")) {
+			export(iFile, oFile);
+		}else{
+			return;
 		}
 	}
 	
 	private void export(File iFile, File oFile) throws IOException{
 		StringBuffer msg = new StringBuffer();
 
-        PrintWriter ps = new PrintWriter(new OutputStreamWriter(new FileOutputStream(oFile.getAbsolutePath(), false), "utf8"));
+		boolean isAuthor = false;
+		boolean isTestCase = false;
+		boolean isDate = false;
+		
+        PrintWriter ps = new PrintWriter(new OutputStreamWriter(new FileOutputStream(oFile.getAbsolutePath(), true), "utf8"));
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(iFile.getAbsolutePath()), "gbk"));
         
         String line = br.readLine();
         while (line != null) {
-        	
-        	if(line.contains("@")){
-        		msg.append(line).append("\r\n");
+        	if(line.contains("@Author")){        
+        		line = line.substring(line.indexOf("@Author") + 7);
+        		msg.append(line).append("\t");
+        		isAuthor = true;
+        		System.out.println(line);
+        	}else if(line.contains("@TestCase")){
+        		if(!isAuthor) msg.append("\t");
+        		line = line.substring(line.indexOf("@TestCase") + 9);
+        		msg.append(line).append("\t");
+        		isAuthor = true;
+        		isTestCase = true;
+        		System.out.println(line);
+        	}else if(line.contains("@Data")){
+        		if(!isAuthor) msg.append("\t");
+        		if(!isTestCase) msg.append("\t");
+        		line = line.substring(line.indexOf("@Data") + 5);
+        		msg.append(line).append("\t");
+        		isAuthor = true;
+        		isTestCase = true;
+        		isDate = true;
         		System.out.println(line);
         	}
-            line = br.readLine();
+        	else if(line.contains("@SuppressWarnings")){
+        		msg.append("\n");
+        		isAuthor = false;
+        		isTestCase = false;
+        		isDate = false;
+        		break;
+        	}
+        	line = br.readLine();
         }
         ps.write(msg.toString());
         br.close();
